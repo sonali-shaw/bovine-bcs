1c013f66d4b0504cdbffd6a4112a0818141fedf7
Sonali Shaw <sshaw@ucls.uchicago.edu>
2024-07-15 14:09:02 -0500

    CNN model (slow)

--- dataset.py
+++ dataset.py
@@ -35,7 +35,8 @@ class CowsDataset(Dataset):
     self.dataset = []
     self.padded_imgs = []
     self.labels = ['200', '225', '250', '275', '300', '325', '350', '375', '400', '425', '450']
-    # self.labels = {label: idx for idx, label in enumerate(self.labels)}
+    self.labels = {label: idx for idx, label in enumerate(self.labels)}
+    # self.labels = [200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450]
     # self.labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     if transform: self.transform = transform
 
@@ -84,21 +85,22 @@ class CowsDataset(Dataset):
         if name[0] != ".":
             file_np = np.load(os.path.join(self.root_dir, name), allow_pickle=True)
             label = label_dict[search_name(name)]
+            mapped_lbl = self.labels[str(label)]
             if mode == 'permute':
                 for i in range(50): # because there are 50 frames in each video
                     rand_mode = modes[random.randint(0, 5)]
                     all_imgs.append(file_np[rand_mode][i])
-                    corr_labels.append(label)
+                    corr_labels.append(mapped_lbl)
             elif type(mode) == tuple:
                 for i in range(50):
                     rand_mode = modes[[random.randint(0, len(modes)-1)]]
                     all_imgs.append(file_np[rand_mode][i])
-                    corr_labels.append(label)
+                    corr_labels.append(mapped_lbl)
             else:
                 video = np.array(file_np[mode])
                 for img in video:
                     all_imgs.append(img)
-                    corr_labels.append(label)
+                    corr_labels.append(mapped_lbl)
 
     frame_length, channel_length = __get_frame_and_channel_length(all_imgs)
     for i in range(len(all_imgs)):
@@ -115,27 +117,36 @@ class CowsDataset(Dataset):
         return item_transformed, label
     return image, label
 
-start_time = time.time()
-full_dataset = CowsDataset("/Users/safesonali/Desktop/DSI-2024/depth_processed",
-                               "/Users/safesonali/Desktop/DSI-2024/bcs_dict.csv",
-                               mode='gradangle',
-                            transform=transforms.ToTensor())
-end_time = time.time()
-print(f"time taken: {end_time - start_time}")
-print(full_dataset[0])
+if __name__ == '__main__':
+    pass
+    # start_time = time.time()
+    # full_dataset = CowsDataset("/Users/safesonali/Desktop/DSI-2024/depth_processed",
+    #                                "/Users/safesonali/Desktop/DSI-2024/bcs_dict.csv",
+    #                                mode='gradangle',
+    #                             transform=transforms.ToTensor())
+    # end_time = time.time()
+    # print(f"time taken: {end_time - start_time}")
+    # # print(full_dataset[0][0])
+    # print(len(full_dataset[0][0][0]))
+    # print(len(full_dataset[0][0][0][0]))
 
-# visualization
-# frames = []
-# for i in range(200):
-#     frames.append(full_dataset[i][0])
-#
-# fig, ax = plt.subplots()
-# def update_frame(frame_idx):
-#   frame = frames[frame_idx]
-#   ax.cla()
-#   img = ax.imshow(frame)
-#   return img
-#
-# animation = FuncAnimation(fig, update_frame, frames=len(frames), interval=50)
-# plt.show()
+
+    # for i in range(10):
+    #     print("-------------------")
+    #     print(full_dataset[i])
+
+    # visualization
+    # frames = []
+    # for i in range(200):
+    #     frames.append(full_dataset[i][0])
+    #
+    # fig, ax = plt.subplots()
+    # def update_frame(frame_idx):
+    #   frame = frames[frame_idx]
+    #   ax.cla()
+    #   img = ax.imshow(frame)
+    #   return img
+    #
+    # animation = FuncAnimation(fig, update_frame, frames=len(frames), interval=50)
+    # plt.show()
 
--- main.py
+++ main.py
@@ -10,7 +10,11 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
-
+import requests
+from pathlib import Path
+from helper_functions import accuracy_fn
+from timeit import default_timer as timer
+from tqdm.auto import tqdm
 
 if __name__ == '__main__':
 
@@ -19,97 +23,106 @@ if __name__ == '__main__':
         # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    start_time = time.time()
     full_dataset = CowsDataset("/Users/safesonali/Desktop/DSI-2024/depth_processed",
                                "/Users/safesonali/Desktop/DSI-2024/bcs_dict.csv",
                                mode='gradangle',
                                transform=transform)
-    end_time = time.time()
-    print(f"time to create dataset: {end_time - start_time}")
-
 
     train_size = math.floor(0.8*len(full_dataset))
     test_size = len(full_dataset) - train_size
     train_data, test_data = torch.utils.data.random_split(full_dataset, [train_size, test_size])
 
+    BATCH_SIZE = 16
+
     train_dataloader = DataLoader(dataset=train_data,
-                                  batch_size=1,
+                                  batch_size=BATCH_SIZE,
                                   num_workers=1,
                                   shuffle=True)
 
     test_dataloader = DataLoader(dataset=test_data,
-                                 batch_size=1,
+                                 batch_size=BATCH_SIZE,
                                  num_workers=1,
                                  shuffle=False)
 
+    train_features_batch, train_labels_batch = next(iter(train_dataloader))
     class_names = full_dataset.labels
-    BATCH_SIZE = 32
 
-
-    def accuracy_fn(y_true, y_pred):
-        correct = torch.eq(y_true, y_pred).sum().item()  # torch.eq() calculates where two tensors are equal
-        acc = (correct / len(y_pred)) * 100
-        return acc
-
-    # Define model
+    def print_train_time(start: float, end: float, device: torch.device = None):
+        total_time = end - start
+        print(f"Train time on {device}: {total_time:.3f} seconds")
+        return total_time
     class CowBCSCNN(nn.Module):
-        def __init__(self):
-            super(CowBCSCNN, self).__init__()
-            self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
-            self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-            self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-            self.fc1 = nn.Linear(in_features=64 * 58 * 129, out_features=128)
-            self.fc2 = nn.Linear(in_features=128, out_features=len(class_names))
+        def __init__(self, input_channels):
+            super().__init__()
+            self.network = nn.Sequential(
+                nn.Conv2d(in_channels=input_channels, out_channels=32, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
 
-        def forward(self, x):
-            x = self.pool(F.relu(self.conv1(x)))
-            x = self.pool(F.relu(self.conv2(x)))
-            x = x.view(-1, 64 * 58 * 129)
-            x = F.relu(self.fc1(x))
-            x = self.fc2(x)
-            return x
+                nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+
+                nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+
+                nn.Flatten(),
+                nn.Linear(256 * 29 * 64, 1024),
+                nn.ReLU(),
+                nn.Linear(1024, 512),
+                nn.ReLU(),
+                nn.Linear(512, 11)
+            )
+
+        def forward(self, xb):
+            return self.network(xb)
+
+    model_0 = CowBCSCNN(input_channels=1)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+
+    torch.manual_seed(23)
+    train_time_start = timer()
+    epochs = 1
+
+    for epoch in tqdm(range(epochs)):
+        print(f"Epoch: {epoch}\n-------")
+        train_loss = 0
+        for batch, (X, y) in enumerate(train_dataloader):
+            model_0.train()
+            y_pred = model_0(X)
+            loss = loss_fn(y_pred, y)
+            train_loss += loss
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if batch % 10 == 0:
+                print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
+        train_loss /= len(train_dataloader)
+
+        ### Validation
+        test_loss, test_acc = 0, 0
+        model_0.eval()
+        with torch.inference_mode():
+            for X, y in test_dataloader:
+                test_pred = model_0(X)
+                test_loss += loss_fn(test_pred, y)
+                test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+            test_loss /= len(test_dataloader)
+            test_acc /= len(test_dataloader)
+        print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
+
+    train_time_end = timer()
+    total_train_time_model_0 = print_train_time(start=train_time_start,
+                                                end=train_time_end,
+                                                device=str(next(model_0.parameters()).device))
 
 
-    model = CowBCSCNN()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-    # Training loop
-    print("beginning trainiing")
-    start_training_time = time.time()
-
-    train_loss, train_acc = 0, 0
-    # model.to(device)
-    for batch, (X, y) in enumerate(train_dataloader):
-        print(batch)
-        print(X, y)
-        model.train()
-        y_pred = model(X)
-        loss = criterion(y_pred, y)
-        train_loss += loss
-        train_acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if batch % 100 == 0:
-            print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples.")
-    train_loss /= len(train_dataloader)
-    train_acc /= len(train_dataloader)
-    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
-
-    end_training_time = time.time()
-    print(f"time to train: {end_training_time - start_training_time}")
-
-
-    # Evaluation loop
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for images, labels in test_dataloader:
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        print(f'Accuracy of the model on the test images: {100 * correct / total}%')
 
