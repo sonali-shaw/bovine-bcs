fdd0ff73e019a23a9355bc6df1cfbc42bd21c135
Sonali Shaw <sshaw@ucls.uchicago.edu>
2024-07-18 10:11:06 -0500

    regression model

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from helper_functions import accuracy_fn
 from timeit import default_timer as timer
 from tqdm.auto import tqdm
+import torchvision
 
 if __name__ == '__main__':
 
@@ -24,10 +25,13 @@ if __name__ == '__main__':
         transforms.Normalize(mean=[0.5], std=[0.5])
     ])
 
+    start_load = timer()
     full_dataset = CowsDataset("/Users/safesonali/Desktop/DSI-2024/depth_processed",
                                "/Users/safesonali/Desktop/DSI-2024/bcs_dict.csv",
                                mode='gradangle',
                                transform=transform)
+    end_load = timer()
+    print(f"time to load data: {end_load-start_load}")
 
     train_size = math.floor(0.8*len(full_dataset))
     test_size = len(full_dataset) - train_size
@@ -46,10 +50,12 @@ if __name__ == '__main__':
                                  shuffle=False)
 
     train_features_batch, train_labels_batch = next(iter(train_dataloader))
+
     flatten_model = nn.Flatten()
     x = train_features_batch[0]
     output = flatten_model(x)
     class_names = full_dataset.labels
+    # class_names = full_dataset.labels_normalized
 
     def print_train_time(start: float, end: float, device: torch.device = None):
         total_time = end - start
@@ -57,58 +63,37 @@ if __name__ == '__main__':
         return total_time
     class CowBCSCNN(nn.Module):
         def __init__(self, input_channels):
-            super().__init__()
-            self.network = nn.Sequential(
-                nn.Conv2d(in_channels=input_channels, out_channels=32, kernel_size=3, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),
-
-                nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),
-
-                nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),
-
-                nn.Flatten(),
-                nn.Linear(256 * 29 * 64, 1024),
-                nn.ReLU(),
-                nn.Linear(1024, 512),
-                nn.ReLU(),
-                nn.Linear(512, 11)
-            )
+            super(CowBCSCNN, self).__init__()
+            self.resnet = torchvision.models.resnet18(pretrained=True)
+            self.resnet.conv1 = nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            self.resnet.fc = nn.Linear(512, 1)  # assuming 11 classes
 
         def forward(self, xb):
-            return self.network(xb)
+            return self.resnet(xb)
 
     model_0 = CowBCSCNN(input_channels=1)
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+    # loss_fn = nn.CrossEntropyLoss() ## for classification
+    loss_fn = nn.MSELoss() # for regression
+    optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.01)
 
     train_time_start = timer()
     torch.manual_seed(43)
-    epochs = 10
+    epochs = 4
 
     for epoch in tqdm(range(epochs)):
         print(f"Epoch: {epoch+1}\n-------")
         train_loss, train_acc = 0, 0
         for batch, (X, y) in enumerate(train_dataloader):
+            start_batch = timer()
             model_0.train()
             y_pred = model_0(X)
-            loss = loss_fn(y_pred, y)
+            loss = loss_fn(y_pred, y.float())
             train_loss += loss
             train_acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
-            print(f"train acc in loop: {train_acc}")
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            end_batch = timer()
             if batch % 8 == 0:
                 print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
         train_loss /= len(train_dataloader)
@@ -121,9 +106,8 @@ if __name__ == '__main__':
         with torch.inference_mode():
             for X, y in test_dataloader:
                 test_pred = model_0(X)
-                test_loss += loss_fn(test_pred, y)
+                test_loss += loss_fn(test_pred, y.float())
                 test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
-
             test_loss /= len(test_dataloader)
             test_acc /= len(test_dataloader)
             print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
@@ -156,7 +140,7 @@ if __name__ == '__main__':
         with torch.inference_mode():
             for X, y in data_loader:
                 # Send data to the target device
-                X, y = X.to(device), y.to(device)
+                X, y = X.to(device), y.to(device).float()
                 y_pred = model(X)
                 loss += loss_fn(y_pred, y)
                 acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
@@ -168,19 +152,19 @@ if __name__ == '__main__':
                 "model_loss": loss.item(),
                 "model_acc": acc}
 
-    model_0_results = eval_model(model=model_0,
+    model_results = eval_model(model=model_0,
                                  data_loader=test_dataloader,
                                  loss_fn=loss_fn,
                                  accuracy_fn=accuracy_fn,
                                  device=device)
-    print(model_0_results)
+    print(model_results)
 
     # saving
 
     MODEL_PATH = Path("models")
     MODEL_PATH.mkdir(parents=True,exist_ok=True)
 
-    MODEL_NAME = "cowsCNNmodel2.pth"
+    MODEL_NAME = "cowsCNNmodel-REGRESSION.pth"
     MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
 
     print(f"Saving model to: {MODEL_SAVE_PATH}")
